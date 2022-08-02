a8d78ba2cbbb94217c7c554564a697503edf608a
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-08-02 10:57:46 +0200

    Termometer interface using animation and timer to simulate it is slowly changing to the set temperature

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,21 +11,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,84 +26,200 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   int _counter = 0;
+  //customized glow with animation
+  late AnimationController _animationController;
+  late Animation _animation;
+
+  //just to try 'changing temperature' mode
+  late Timer _timer;
+  int currentTemp = 15;
+  int setTemp = 20;
+
+  void startTempChange() {
+    const oneSec = Duration(seconds: 1);
+    _timer = Timer.periodic(
+      oneSec,
+      (Timer timer) {
+        if (currentTemp == setTemp) {
+          setState(() {
+            timer.cancel();
+          });
+        } else {
+          setState(() {
+            if (currentTemp > setTemp) {
+              currentTemp--;
+            } else {
+              currentTemp++;
+            }
+          });
+        }
+      },
+    );
+  }
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
+  String celiousModifier(double value) {
+    final roundedValue = value.ceil().toInt().toString();
+    return '$roundedValue°C';
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+    _animationController.repeat(reverse: true);
+    _animation = Tween(begin: 20.0, end: 35.0).animate(_animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: <Widget>[
+          const Text(
+            'You have pushed the button this many times:',
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            '$_counter',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              segundoSleekCircular(currentTemp.toDouble()),
+              primerSleekCircular(21),
+            ],
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
+
+  Widget nuevoSleekCircular(double percentage) {
+    return const SleekCircularSlider(
+        appearance: CircularSliderAppearance(
+      size: 100,
+      startAngle: 0,
+      angleRange: 360,
+      spinnerMode: true,
+    ));
+  }
+
+  Widget primerSleekCircular(double percentage) {
+    return SleekCircularSlider(
+      onChange: ((value) {
+        log('CHANGING: ${value.toString()}');
+      }),
+      onChangeStart: ((value) {
+        log('START: ${value.toString()}');
+      }),
+      onChangeEnd: ((value) {
+        startTempChange();
+        setTemp = value.toInt();
+        log('END: ${value.toString()}');
+      }),
+      // innerWidget: nuevoSleekCircular,
+      appearance: CircularSliderAppearance(
+        infoProperties: InfoProperties(
+          modifier: celiousModifier,
+          topLabelText: 'Temperature',
+          bottomLabelText: 'Good day',
+        ),
+        customWidths: CustomSliderWidths(
+          progressBarWidth: 15,
+          trackWidth: 0.1,
+          shadowWidth: _animation.value,
+          handlerSize: 5,
+        ),
+        customColors: CustomSliderColors(
+          trackColor: Colors.amberAccent,
+          progressBarColor: Colors.amberAccent,
+          shadowColor: Colors.amberAccent.shade100,
+        ),
+        // spinnerMode: true,
+        size: 250,
+        startAngle: 270,
+        angleRange: 360,
+      ),
+      min: 15,
+      max: 35,
+      initialValue: percentage,
+    );
+  }
+
+  Widget segundoSleekCircular(double percentage) {
+    return SleekCircularSlider(
+      onChange: ((value) {
+        log('CHANGING: ${value.toString()}');
+      }),
+      onChangeStart: ((value) {
+        log('START: ${value.toString()}');
+      }),
+      onChangeEnd: ((value) {
+        log('END: ${value.toString()}');
+      }),
+      appearance: CircularSliderAppearance(
+        infoProperties: InfoProperties(
+          modifier: (double value) {
+            return '';
+          },
+          topLabelText: '',
+          bottomLabelText: '',
+        ),
+        customWidths: CustomSliderWidths(
+          progressBarWidth: 10,
+          trackWidth: 0.1,
+          shadowWidth: _animation.value,
+          handlerSize: 5,
+        ),
+        customColors: CustomSliderColors(
+          trackColor: Colors.amberAccent,
+          progressBarColor: Colors.amberAccent,
+          shadowColor: Colors.amberAccent.shade100,
+        ),
+        // spinnerMode: true,
+        size: 210,
+        startAngle: 270,
+        angleRange: 360,
+      ),
+      min: 15,
+      max: 35,
+      initialValue: percentage,
+    );
+  }
+//
 }
