9444ed37f19e8eef2025f98255906af036169413
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-08-03 15:23:40 +0200

    Animation now working faster when the value is changed and then going back to normal once it reach the value

--- lib/main.dart
+++ lib/main.dart
@@ -36,29 +36,46 @@ class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   int _counter = 0;
   //customized glow with animation
-  late AnimationController _animationController;
-  late Animation _animation;
+  late AnimationController _glowingAnimationController;
+  late Animation _glowingAnimation;
 
   //just to try 'changing temperature' mode
   late Timer _timer;
-  int currentTemp = 15;
-  int setTemp = 20;
+  int animationDuration = 2000;
+  double currentTemp = 15;
+  double setTemp = 20;
 
   void startTempChange() {
     const oneSec = Duration(seconds: 1);
     _timer = Timer.periodic(
       oneSec,
       (Timer timer) {
+        log(currentTemp.toString());
         if (currentTemp == setTemp) {
           setState(() {
+            //here the magice happens
+            _glowingAnimationController.duration =
+                Duration(milliseconds: animationDuration);
+            _glowingAnimationController.repeat(reverse: true);
             timer.cancel();
           });
         } else {
           setState(() {
+            _glowingAnimationController.duration =
+                const Duration(milliseconds: 500);
+            _glowingAnimationController.repeat(reverse: true);
             if (currentTemp > setTemp) {
-              currentTemp--;
+              if (currentTemp - setTemp < 1) {
+                currentTemp = setTemp;
+              } else {
+                currentTemp--;
+              }
             } else {
-              currentTemp++;
+              if (setTemp - currentTemp < 1) {
+                currentTemp = setTemp;
+              } else {
+                currentTemp++;
+              }
             }
           });
         }
@@ -66,32 +83,22 @@ class _MyHomePageState extends State<MyHomePage>
     );
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  String celiousModifier(double value) {
-    final roundedValue = value.ceil().toInt().toString();
-    return '$roundedValue°C';
-  }
-
   @override
   void dispose() {
     _timer.cancel();
+    _glowingAnimationController.dispose();
     super.dispose();
   }
 
   @override
   void initState() {
-    _animationController = AnimationController(
-        duration: const Duration(milliseconds: 2000), vsync: this);
-    _animationController.repeat(reverse: true);
-    _animation = Tween(begin: 20.0, end: 35.0).animate(_animationController)
-      ..addListener(() {
-        setState(() {});
-      });
+    _glowingAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: animationDuration),
+    );
+    _glowingAnimationController.repeat(reverse: true);
+    _glowingAnimation =
+        Tween(begin: 20.0, end: 35.0).animate(_glowingAnimationController);
     super.initState();
   }
 
@@ -112,34 +119,60 @@ class _MyHomePageState extends State<MyHomePage>
             textAlign: TextAlign.center,
             style: Theme.of(context).textTheme.headline4,
           ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              segundoSleekCircular(currentTemp.toDouble()),
-              primerSleekCircular(21),
-            ],
-          ),
+          litoThermometer(),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.question_mark),
       ),
     );
   }
 
-  Widget nuevoSleekCircular(double percentage) {
-    return const SleekCircularSlider(
-        appearance: CircularSliderAppearance(
-      size: 100,
-      startAngle: 0,
-      angleRange: 360,
-      spinnerMode: true,
-    ));
+  Widget litoThermometer() {
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: [
+        innerSleekCircularSlider(currentTemp),
+        outerSleekCircularSlider(setTemp),
+      ],
+    );
   }
 
-  Widget primerSleekCircular(double percentage) {
+  CircularSliderAppearance litoCSA({
+    bool isInner = false,
+    int oTemperature = 20,
+    int iTemperature = 19,
+  }) {
+    return CircularSliderAppearance(
+      infoProperties: InfoProperties(
+        modifier: (value) {
+          final roundedValue = value.ceil().toInt().toString();
+          return isInner ? '' : '$roundedValue°C';
+        },
+        topLabelText: isInner ? '' : 'Temperature',
+        bottomLabelText: isInner ? '' : 'Good day',
+      ),
+      customWidths: CustomSliderWidths(
+        progressBarWidth: isInner ? 10 : 15,
+        trackWidth: isInner ? 0.1 : 3,
+        shadowWidth: isInner ? 0 : _glowingAnimation.value,
+        handlerSize: isInner ? 0 : 5,
+      ),
+      customColors: CustomSliderColors(
+        trackColor: const Color.fromARGB(119, 189, 189, 189),
+        progressBarColor: isInner ? Colors.yellow[900] : Colors.amberAccent,
+        shadowColor: Colors.amberAccent.shade100,
+      ),
+      // spinnerMode: true,
+      size: !isInner ? 250 : 215,
+      startAngle: 270,
+      angleRange: 360,
+    );
+  }
+
+  Widget outerSleekCircularSlider(double temperature) {
     return SleekCircularSlider(
       onChange: ((value) {
         log('CHANGING: ${value.toString()}');
@@ -149,76 +182,27 @@ class _MyHomePageState extends State<MyHomePage>
       }),
       onChangeEnd: ((value) {
         startTempChange();
-        setTemp = value.toInt();
+        setTemp = value;
         log('END: ${value.toString()}');
       }),
       // innerWidget: nuevoSleekCircular,
-      appearance: CircularSliderAppearance(
-        infoProperties: InfoProperties(
-          modifier: celiousModifier,
-          topLabelText: 'Temperature',
-          bottomLabelText: 'Good day',
-        ),
-        customWidths: CustomSliderWidths(
-          progressBarWidth: 15,
-          trackWidth: 0.1,
-          shadowWidth: _animation.value,
-          handlerSize: 5,
-        ),
-        customColors: CustomSliderColors(
-          trackColor: Colors.amberAccent,
-          progressBarColor: Colors.amberAccent,
-          shadowColor: Colors.amberAccent.shade100,
-        ),
-        // spinnerMode: true,
-        size: 250,
-        startAngle: 270,
-        angleRange: 360,
+      appearance: litoCSA(
+        isInner: false,
       ),
       min: 15,
       max: 35,
-      initialValue: percentage,
+      initialValue: temperature,
     );
   }
 
-  Widget segundoSleekCircular(double percentage) {
+  Widget innerSleekCircularSlider(double temperature) {
     return SleekCircularSlider(
-      onChange: ((value) {
-        log('CHANGING: ${value.toString()}');
-      }),
-      onChangeStart: ((value) {
-        log('START: ${value.toString()}');
-      }),
-      onChangeEnd: ((value) {
-        log('END: ${value.toString()}');
-      }),
-      appearance: CircularSliderAppearance(
-        infoProperties: InfoProperties(
-          modifier: (double value) {
-            return '';
-          },
-          topLabelText: '',
-          bottomLabelText: '',
-        ),
-        customWidths: CustomSliderWidths(
-          progressBarWidth: 10,
-          trackWidth: 0.1,
-          shadowWidth: _animation.value,
-          handlerSize: 5,
-        ),
-        customColors: CustomSliderColors(
-          trackColor: Colors.amberAccent,
-          progressBarColor: Colors.amberAccent,
-          shadowColor: Colors.amberAccent.shade100,
-        ),
-        // spinnerMode: true,
-        size: 210,
-        startAngle: 270,
-        angleRange: 360,
+      appearance: litoCSA(
+        isInner: true,
       ),
       min: 15,
       max: 35,
-      initialValue: percentage,
+      initialValue: temperature,
     );
   }
 //
